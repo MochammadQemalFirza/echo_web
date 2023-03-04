52011e93cc5dc0ff5c6621ada9142d01c860420c
Mochammad Qemal Firza <kemalfirza@gmail.com>
2023-03-04 08:55:21 +0700

    set db

--- server.go
+++ server.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/MochammadQemalFirza/echo_web/config"
 	"github.com/MochammadQemalFirza/echo_web/routes"
 )
 
 func main() {
-
+	config.CreateCon()
 	e := routes.InitRouter()
 
 	e.Logger.Fatal(e.Start(":8081"))
